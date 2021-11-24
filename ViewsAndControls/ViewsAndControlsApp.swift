362ed70c3d7808ccacf4e544171e4bd6e519e0e1
Russell Gordon <rgordon@lcs.on.ca>
2021-11-23 19:01:53 -0500

    Initial Commit

---
+++ ViewsAndControls/ViewsAndControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndControlsApp.swift
+//  ViewsAndControls
+//
+//  Created by Russell Gordon on 2021-11-23.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
